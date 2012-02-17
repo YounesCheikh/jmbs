abdb1b04bd5acbaa4f533c653d57fa69e24decda
Younes CHEIKH <younes.cheikh@gmail.com>
2012-02-17 15:10:19 +0000

    pas de commentaire

--- src/rmi_test_alpha/InterConnect.java
+++
@@ -1,7 +0,0 @@
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface InterConnect extends Remote {
-	public boolean trueIdentity(UserIdentity ui) throws RemoteException;
-}
