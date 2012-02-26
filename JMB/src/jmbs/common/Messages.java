67fd798df9defb034e7d8d5abdfdfca8cdd7f0f8
Younes CHEIKH <younes.cheikh@gmail.com>
2012-02-26 15:35:16 +0000

    + v0.1 Message class added

---
+++ JMB/src/jmbs/common/Messages.java
@@ -0,0 +1,62 @@
+package jmbs.common;
+
+import java.sql.Date;
+
+public class Messages {
+
+	private int idMessage;
+	private User owner;
+	private String message;
+	private Date datetime;
+	/**
+	 * @return the idMessage
+	 */
+	public int getIdMessage() {
+		return idMessage;
+	}
+	/**
+	 * @param idMessage the idMessage to set
+	 */
+	public void setIdMessage(int idMessage) {
+		this.idMessage = idMessage;
+	}
+	/**
+	 * @return the owner
+	 */
+	public User getOwner() {
+		return owner;
+	}
+	/**
+	 * @param owner the owner to set
+	 */
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the datetime
+	 */
+	public Date getDatetime() {
+		return datetime;
+	}
+	/**
+	 * @param datetime the datetime to set
+	 */
+	public void setDatetime(Date datetime) {
+		this.datetime = datetime;
+	}
+	
+	
+
+}
