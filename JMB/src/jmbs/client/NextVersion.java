eb870fb864fe50fbfb8a048bf5101cc3d8b54d26
Younes CHEIKH <younes.cheikh@gmail.com>
2012-03-12 23:21:18 +0000

    a dialog window , just for test :-)

---
+++ JMB/src/jmbs/client/NextVersion.java
@@ -0,0 +1,105 @@
+package jmbs.client;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class NextVersion extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7095502745092240625L;
+	private final JPanel contentPanel = new JPanel();
+	private JPanel buttonPane;
+	private JLabel lblWeAreVery;
+
+	/**
+	 * Create the dialog.
+	 */
+	public NextVersion() {
+		setBounds(100, 100, 450, 189);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		{
+			lblWeAreVery = new JLabel("We are very very very very very very very very very Sorry!");
+		}
+		{
+			buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(contentPanel, GroupLayout.PREFERRED_SIZE, 450, GroupLayout.PREFERRED_SIZE)
+						.addComponent(buttonPane, GroupLayout.PREFERRED_SIZE, 450, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(contentPanel, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(buttonPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(207))
+		);
+		JLabel lblThisButtonWill = new JLabel("This button will be available in the next version!! O_o");
+		JLabel lblCloseThisAnd = new JLabel("Close this, and don't try to compile this program again!");
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGap(44)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblCloseThisAnd)
+						.addComponent(lblThisButtonWill)
+						.addComponent(lblWeAreVery))
+					.addContainerGap(111, Short.MAX_VALUE))
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGap(14)
+					.addComponent(lblWeAreVery)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblThisButtonWill)
+					.addGap(18)
+					.addComponent(lblCloseThisAnd)
+					.addContainerGap(19, Short.MAX_VALUE))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		getContentPane().setLayout(groupLayout);
+	}
+
+}
