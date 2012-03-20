523a36e772a99f2387d250194531cbc316835af1
Younes CHEIKH <younes.cheikh@gmail.com>
2012-03-20 18:20:45 +0000

    Set this frame as undecorated frame and enable the mouseDragged() to let user move this frame

--- JMB/src/jmbs/client/Graphics/NewMessageFrame.java
+++ JMB/src/jmbs/client/Graphics/NewMessageFrame.java
@@ -10,7 +10,6 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JScrollPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.JProgressBar;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.sql.Date;
@@ -24,9 +23,9 @@ import javax.swing.border.TitledBorder;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
+
 public class NewMessageFrame extends JFrame {
-	
-	
+
 	/**
 	 * 
 	 */
@@ -36,27 +35,28 @@ public class NewMessageFrame extends JFrame {
 	private JTextArea textArea;
 	private String newMsgStr;
 	private static Point point = new Point();
+
 	/**
 	 * Create the frame.
 	 */
 	public NewMessageFrame(final TimeLinePanel tlpanel) {
-		
+
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				point.x = e.getX();
-		        point.y = e.getY();
+				point.y = e.getY();
 			}
 		});
-		
+
 		addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) {
 				Point p = getLocation();
-		        setLocation(p.x + e.getX() - point.x, p.y + e.getY() - point.y);
+				setLocation(p.x + e.getX() - point.x, p.y + e.getY() - point.y);
 			}
 		});
-		
+
 		newMsgStr = new String();
 		setTitle("Write new message");
 		setAlwaysOnTop(true);
@@ -66,21 +66,24 @@ public class NewMessageFrame extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setViewportBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
+		scrollPane.setViewportBorder(new TitledBorder(null, "",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		scrollPane
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
 		JButton btnSend = new JButton("Send");
 		btnSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				newMsgStr = new String(textArea.getText());
 				textArea.setText("");
-				tlpanel.putMessage(new MsgPanel(new Message(new User("Younes", "CHEIKH", "test@test.com"), "", newMsgStr, new Date(0))));
+				tlpanel.putMessage(new MsgPanel(new Message(new User("Younes",
+						"CHEIKH", "test@test.com"), "", newMsgStr, new Date(0))));
 				setVisible(false);
 			}
 		});
-		
+
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -88,35 +91,47 @@ public class NewMessageFrame extends JFrame {
 			}
 		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnCancel, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 148, Short.MAX_VALUE)
-					.addComponent(btnSend)
-					.addContainerGap())
-				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 310, Short.MAX_VALUE)
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 156, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnSend, GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
-						.addComponent(btnCancel, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
-		);
-		
+		gl_contentPane.setHorizontalGroup(gl_contentPane
+				.createParallelGroup(Alignment.TRAILING)
+				.addGroup(
+						gl_contentPane
+								.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(btnCancel,
+										GroupLayout.PREFERRED_SIZE, 75,
+										GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED,
+										148, Short.MAX_VALUE)
+								.addComponent(btnSend).addContainerGap())
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 310,
+						Short.MAX_VALUE));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(
+				Alignment.TRAILING).addGroup(
+				gl_contentPane
+						.createSequentialGroup()
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE,
+								156, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(
+								gl_contentPane
+										.createParallelGroup(Alignment.LEADING)
+										.addComponent(btnSend,
+												GroupLayout.DEFAULT_SIZE, 42,
+												Short.MAX_VALUE)
+										.addComponent(btnCancel,
+												GroupLayout.PREFERRED_SIZE, 42,
+												GroupLayout.PREFERRED_SIZE))
+						.addContainerGap()));
+
 		textArea = new JTextArea();
 		textArea.setBackground(new Color(245, 245, 245));
 		scrollPane.setViewportView(textArea);
 		contentPane.setLayout(gl_contentPane);
 	}
-	
+
 	/**
-	 * we'll use this in future 
+	 * we'll use this in future
+	 * 
 	 * @return
 	 */
 	public String getMsg() {
