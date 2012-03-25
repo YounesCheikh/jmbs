146fabd6832c9c1a97effcaeb171a5bb81d6f134
Younes CHEIKH <younes.cheikh@gmail.com>
2012-03-25 19:21:49 +0000

    tables.sql Added avatar table

--- JMB/tables.sql
+++ JMB/tables.sql
@@ -62,7 +62,7 @@ CREATE TABLE message ( /* Table des messages */
 	content varchar(600),
 	time timestamp, 
 	iduser integer NOT NULL,
-	title varchar(100);
+	title varchar(100),
 
 CONSTRAINT pk_message PRIMARY KEY (idmessage),
 CONSTRAINT fk_message_user FOREIGN KEY (iduser) REFERENCES users (iduser)
@@ -74,7 +74,18 @@ CREATE TABLE follows ( /* Table des suiveurs */
 
 CONSTRAINT pk_f PRIMARY KEY (follower,followed),	
 CONSTRAINT fk_follower_user FOREIGN KEY (follower) REFERENCES users (iduser), 
-CONSTRAINT fk_followed_user FOREIGN KEY (followed) REFERENCES users (iduser),
+CONSTRAINT fk_followed_user FOREIGN KEY (followed) REFERENCES users (iduser)
 );
+
+CREATE TABLE avatar (
+	idavatar integer NOT NULL,
+	url varchar(500),
+	iduser integer NOT NULL,
+
+CONSTRAINT pk_av PRIMARY KEY (idavatar),
+CONSTRAINT fk_av_user FOREIGN KEY (iduser) REFERENCES users (iduser),
+);
+
+
 
 
