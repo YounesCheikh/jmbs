09ab7f1b9a8cfa26dda98790d6470427ebbc9548
Younes CHEIKH <younes.cheikh@gmail.com>
2012-04-06 13:08:58 +0000

    Pictures manager

---
+++ JMB/src/jmbs/server/PictureDAO.java
@@ -0,0 +1,138 @@
+/**
+ * JMBS: Java Micro Blogging System
+ *
+ * Copyright (C) 2012  
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @author Younes CHEIKH http://cyounes.com
+ * @author Benjamin Babic http://bbabic.com
+ * 
+ */
+
+package jmbs.server;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.imageio.ImageIO;
+
+import jmbs.common.User;
+
+public class PictureDAO extends DAO {
+	private static final long serialVersionUID = -3486687583564090822L;
+	private final static String DEFAULT_SEPARATOR = File.pathSeparator ;
+	private final static String DEFAULT_PATH = "/";
+	private final static String DEFAULT_IMAGE_FORMAT = "jpg";
+	
+	public PictureDAO(Connection c) {
+		super(c);
+	}
+	
+	public BufferedImage getPicture(String path){
+		BufferedImage img = null;
+		
+		try {
+			img = ImageIO.read(new File(path));
+		} catch (IOException e0) {
+			System.err.println("Error on filepath no files named: " + path);
+		} 
+		
+		return img;
+	}
+	
+	public String getRepertoryPath(int userid){
+		return (DEFAULT_PATH + DEFAULT_SEPARATOR + String.valueOf(userid) + DEFAULT_SEPARATOR);
+	}
+	
+	public String getAvatarPath(int userid, String name){
+		return (getRepertoryPath(userid) + name);
+	}
+	
+	public String getName(String path){
+		return path.substring(path.lastIndexOf(DEFAULT_SEPARATOR)+1);
+	}
+	
+	public BufferedImage getAvatar(int iduser, String pic){
+		return (getPicture(getAvatarPath(iduser,pic)));
+	}
+	
+	public BufferedImage getAvatar(User u){
+		return (getAvatar(u.getId(),u.getPic()));
+	}
+	
+	/**
+	 * Creates the image on the server.
+	 * 
+	 * @param u User who uploads the image
+	 * @param img Image to upload
+	 * @param nom Image name
+	 * @return true if the image was saved - false if not.
+	 */
+	public boolean setAvatar(User u, BufferedImage img, String nom){
+		String path = getRepertoryPath(u.getId()) + nom;
+		createUserRepertory(u.getId());
+		boolean ret = false;
+		
+		File f = new File(path);
+		
+		try {
+			ImageIO.write(img, DEFAULT_IMAGE_FORMAT, f);
+			ret = true;
+		} catch (IOException e) {
+			System.out.println("Image cannot be saved");
+		}
+		return ret;
+	}
+	
+	public boolean createUserRepertory(User u){
+		return createUserRepertory(u.getId());
+	}
+	
+	public boolean createUserRepertory(int userid){
+		File f = new File(getRepertoryPath(userid));
+		boolean ret = false;
+		
+		if (!f.exists()){
+			ret = f.mkdir();
+		}
+		
+		return ret;
+	}
+	
+	public boolean deleteUserRepertory(User u){
+		return deleteUserRepertory(u.getId());
+	}
+	
+	public boolean deleteUserRepertory(int userid){
+		boolean ret = false;
+		File f = new File(getRepertoryPath(userid));
+		
+		if (f.isDirectory()){
+			File[] subfiles = f.listFiles();
+			String subfileName = new String ();
+		
+			for (File i:subfiles){
+				subfileName = i.getName();
+				i.delete();
+				System.out.println(subfileName + " deleted.");
+			}
+			if (ret=f.delete()) System.out.println(f.getName() + " deleted.");
+		}else {
+			System.err.println("This user has no avatar repertory.");
+		}
+		
+		return ret;
+	}
+
+}
