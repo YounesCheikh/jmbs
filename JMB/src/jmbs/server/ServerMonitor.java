73e48bb19f2537cbe44883954e2bf55d7bd0ae44
Younes CHEIKH <younes.cheikh@gmail.com>
2012-04-18 22:02:54 +0000

    ServerMonitor

---
+++ JMB/src/jmbs/server/ServerMonitor.java
@@ -0,0 +1,71 @@
+package jmbs.server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public final class ServerMonitor {
+
+	private static ServerMonitor instance = null;
+	HashMap<String, ConnectionInformation> activeConnections = new HashMap<String, ConnectionInformation>();
+	HashMap<Integer,String> activeAccounts = new HashMap<Integer,String>();
+	Security s = new SecurityDAO(new Connect().getConnection());
+
+	private ServerMonitor() {
+	}
+
+	public final static ServerMonitor getInstance() {
+		if (instance == null) {
+			synchronized (Configuration.class) {
+				if (instance == null)
+					instance = new ServerMonitor();
+			}
+		}
+		return instance;
+	}
+
+	public boolean addConnection(ConnectionInformation ci) throws SecurityException{
+		String ip = ci.getIp();
+		
+		if (!isConnectionActive(ip)) { 
+			if (!s.isBanned(ip)){
+				activeConnections.put(ip, ci);
+			}else {
+				throw new SecurityException(s.getBanInformation(ip).toString());
+				}			
+		} else
+			throw new SecurityException("You are trying to set 2 connections from de same ip adress at the same time.\n Please log of and try again.");
+		return true;
+	}
+	
+	public void connectUserUnderIp(int userid, String ip){
+		activeAccounts.put(userid, ip);
+	}
+
+	public int getActiveConnections() {
+		return activeConnections.size();
+	}
+	
+	public boolean isAcountActive(int iduser){
+		return activeAccounts.containsKey(iduser);
+	}
+	
+	public boolean isConnectionActive(String ip){
+		return activeConnections.containsKey(ip);
+	}
+
+	public boolean connectToAccount(String ip) {
+		ConnectionInformation ci = activeConnections.get(ip);
+		ci.connectionAttempt();
+		
+		return (s.isConnectionAuthorized(ci));	
+	}
+
+	public void closeConnection(String ip) {
+		activeConnections.remove(ip);
+	}
+	
+	public void logOff (int userid){
+		activeAccounts.remove(userid);
+	}
+	
+}
