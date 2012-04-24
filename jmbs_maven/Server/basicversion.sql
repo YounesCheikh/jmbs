6eef066a224816461c882c56be1bfeb525349613
Younes CHEIKH <younes.cheikh@gmail.com>
2012-04-24 00:02:15 +0000

    the Latest stable version of DB

--- jmbs_maven/Server/basicversion.sql
+++ jmbs_maven/Server/basicversion.sql
@@ -10,70 +10,71 @@ Version: 1.1
 
 
 
-CREATE TABLE users ( /* Table des utilisateurs */
-	iduser serial,
-	name varchar(20),
-	forename varchar(20),
-	email varchar(200),
-	pass varchar(200),
-    	picture varchar(200),
-	authlvl integer,
-
-CONSTRAINT pk_users PRIMARY KEY (iduser)
+CREATE TABLE users
+(
+  iduser serial NOT NULL,
+  "name" character varying(20) NOT NULL,
+  forename character varying(20) NOT NULL,
+  email character varying(200) NOT NULL,
+  pass character varying(200) NOT NULL,
+  picture character varying(200),
+  authlvl integer NOT NULL,
+  CONSTRAINT pk_users PRIMARY KEY (iduser)
 );
 
-CREATE TABLE projects (
-	idproject serial,
-	idowner integer NOT NULL,
-	name varchar(60),
-	status integer,
-	
-CONSTRAINT pk_projects PRIMARY KEY (idproject),
-CONSTRAINT fk_projects_user FOREIGN KEY (idowner) REFERENCES users (iduser)
+CREATE TABLE projects
+(
+  idproject serial NOT NULL,
+  idowner integer NOT NULL,
+  "name" character varying(20),
+  status integer,
+  nbsuscribers integer,
+  CONSTRAINT pk_projects PRIMARY KEY (idproject)
 );
 
-CREATE TABLE message ( /* Table des messages */
-	idmessage serial,
-	content varchar(600),
-	time timestamp, 
-	iduser integer NOT NULL,
-	idproject integer,
-
-CONSTRAINT pk_message PRIMARY KEY (idmessage),
-CONSTRAINT fk_message_user FOREIGN KEY (iduser) REFERENCES users (iduser),
-CONSTRAINT fk_message_project FOREIGN KEY (idproject) REFERENCES projects (idproject)
+CREATE TABLE "connection"
+(
+  idconnection serial NOT NULL,
+  ip character varying(20) NOT NULL,
+  iduser integer,
+  begintime timestamp without time zone,
+  endtime timestamp without time zone,
+  CONSTRAINT pk_connect PRIMARY KEY (idconnection)
 );
 
-CREATE TABLE follows ( /* Table des suiveurs */
-	follower integer NOT NULL,
-	followed integer NOT NULL,
+CREATE TABLE follows
+(
+  follower integer NOT NULL,
+  followed integer NOT NULL,
+  CONSTRAINT pk_f PRIMARY KEY (follower, followed)
+);
 
-CONSTRAINT pk_f PRIMARY KEY (follower,followed),	
-CONSTRAINT fk_follower_user FOREIGN KEY (follower) REFERENCES users (iduser), 
-CONSTRAINT fk_followed_user FOREIGN KEY (followed) REFERENCES users (iduser)
+CREATE TABLE message
+(
+  idmessage serial NOT NULL,
+  "content" character varying(600),
+  "time" timestamp without time zone,
+  iduser integer NOT NULL,
+  idproject integer,
+  edittime timestamp without time zone,
+  idedituser integer,
+  CONSTRAINT pk_message PRIMARY KEY (idmessage),
+  CONSTRAINT fk_message_edit FOREIGN KEY (idedituser)
+      REFERENCES users (iduser),
+  CONSTRAINT fk_message_pj FOREIGN KEY (idproject)
+      REFERENCES projects (idproject),
+  CONSTRAINT fk_message_u FOREIGN KEY (iduser)
+      REFERENCES users (iduser)
+);
+
+CREATE TABLE participate
+(
+  idproject integer NOT NULL,
+  iduser integer NOT NULL,
+  authlvl integer,
+  CONSTRAINT pk_participate PRIMARY KEY (idproject, iduser)
 );
 
 
 
-CREATE TABLE participate (
-	idproject integer NOT NULL,
-	iduser integer NOT NULL,
-	authlvl integer,
-
-CONSTRAINT pk_participate PRIMARY KEY (idproject,iduser),
-CONSTRAINT fk_part_user FOREIGN KEY (iduser) REFERENCES users (iduser), 
-CONSTRAINT fk_part_project FOREIGN KEY (idproject) REFERENCES projects (idproject)
-);
-
-
-CREATE TABLE banned (
-	ip varchar(20) UNIQUE,
-	lifeban int,
-	expiration Timestamp,
-	reason varchar(500),
-
-CONSTRAINT pk_banned PRIMARY KEY (ip)
-);
-
-CREATE VIEW timeline AS SELECT * FROM message WHERE idproject IS NULL;
 
