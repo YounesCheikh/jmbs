4cdfdb463ba10d482b4616329d087588e9e573b0
Younes CHEIKH <younes.cheikh@gmail.com>
2012-05-06 14:12:39 +0000

    Removing ConnectionPanel and RegistrationPanel

--- jmbs_maven/Client/src/main/java/jmbs/client/Graphics/ConnectionFrame.java
+++ jmbs_maven/Client/src/main/java/jmbs/client/Graphics/ConnectionFrame.java
@@ -17,12 +17,38 @@
 
 package jmbs.client.Graphics;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
+import jmbs.client.ClientRequests;
+import jmbs.client.CurrentUser;
+import jmbs.client.ServerConnection;
 import jmbs.client.DataTreatment.FramesConf;
+import jmbs.client.DataTreatment.HashPassword;
+import jmbs.client.DataTreatment.LoginTreatment;
+import jmbs.client.Graphics.images.ImagePanel;
+import jmbs.client.Graphics.messages.TimeLinePanel;
+import jmbs.common.User;
+import net.miginfocom.swing.MigLayout;
 
 public class ConnectionFrame extends JFrame {
 
@@ -30,6 +56,22 @@ public class ConnectionFrame extends JFrame {
 	 * Display a frame contain the connection panel
 	 */
 	private static final long serialVersionUID = 6941716821811760066L;
+	private JPanel panel;
+	private JTextField emailTextField;
+	private JPasswordField passwordField;
+	private JCheckBox chckbxRememberMe;
+	private boolean savedIdentity = false;
+	private boolean passwordChanged = false;
+	private final LoginTreatment lt = new LoginTreatment();
+	private JLabel respLabel; /*
+							 * this will be used to say to the user if there is
+							 * a wrong password or email
+							 */
+	private RegisterFrame rframe;
+	/**
+	 * the main window
+	 */
+	private MainWindow w;
 
 	/**
 	 * Create the frame.
@@ -54,9 +96,191 @@ public class ConnectionFrame extends JFrame {
 		// this.setLocationRelativeTo(null);
 		FramesConf.centerThisFrame(this);
 
-		ConnectionPanel cp = new ConnectionPanel(w, this);
+		JPanel cp = new JPanel();
+
+		this.w = w;
+		cp.setLayout(new BorderLayout());
+		JLabel lblConnectToJmbs = new JLabel("Connect to JMBS");
+		lblConnectToJmbs.setFont(new Font("Dialog", Font.BOLD, 16));
+
+		JLabel lblEmailAdress = new JLabel("Email Adress:");
+
+		JLabel lblPassword = new JLabel("Password:");
+
+		emailTextField = new JTextField();
+		emailTextField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				savedIdentity = false;
+				passwordChanged = false;
+			}
+		});
+		emailTextField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (lt.isSaved(emailTextField.getText()) != null) {
+					passwordField.setText("********");
+					savedIdentity = true;
+				}
+			}
+		});
+		emailTextField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				checkConnection();
+			}
+		});
+		emailTextField.setColumns(20);
+
+		passwordField = new JPasswordField();
+		passwordField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (e.getKeyChar() != '\n') {
+					passwordChanged = true;
+				}
+			}
+		});
+		passwordField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+
+			}
+		});
+		passwordField.setColumns(20);
+		passwordField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				checkConnection();
+			}
+		});
+
+		respLabel = new JLabel(" ");
+
+		chckbxRememberMe = new JCheckBox("Remember me");
+
+		JLabel lblRegister = new JLabel("don't have account?");
+
+		ImagePanel logopanel = new ImagePanel("/img/jmbslogo_small.png");
+
+		JButton btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rframe = new RegisterFrame();
+				rframe.setVisible(true);
+			}
+		});
+		btnRegister.setHorizontalAlignment(SwingConstants.LEFT);
+		btnRegister.setForeground(Color.BLUE);
+		btnRegister.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		panel = new JPanel();
+		panel.setLayout(new MigLayout(
+				"",
+				"[63px][12px][10px][12px][102px,grow][6px][104px][][6px][96px]",
+				"[20px][136px][28px][28px][23px][30px][12px][][][][36px][grow]"));
+		btnRegister.setBorderPainted(false);
+		panel.add(lblPassword, "cell 0 3,alignx left,aligny center");
+		panel.add(passwordField, "cell 4 3 6 1,aligny top");
+		panel.add(lblConnectToJmbs, "cell 4 0 3 1,alignx center,aligny top");
+		panel.add(lblRegister, "cell 2 5 3 1,alignx left,aligny center");
+		panel.add(btnRegister, "cell 6 5,growx,aligny top");
+
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!lt.isMailCorrect(emailTextField.getText())) {
+					respLabel.setText("Enter a valide email please!");
+					respLabel.setForeground(Color.red);
+					// putElement(0, 3, respLabel);
+				} else if (!lt.hasMinLenght(passwordField.getPassword().length)) {
+					respLabel.setText("password less than 4 chars !");
+					respLabel.setForeground(Color.red);
+					// putElement(0, 3, respLabel);
+				} else {
+					checkConnection();
+				}
+			}
+		});
+		panel.add(btnConnect, "cell 6 7,alignx left,aligny bottom");
+
+		JSeparator separator = new JSeparator();
+		panel.add(separator, "cell 0 8 10 1,growx,aligny top");
+		panel.add(respLabel, "cell 0 10 10 1,grow");
+		panel.add(chckbxRememberMe, "cell 4 4 3 1,alignx center,aligny top");
+		panel.add(lblEmailAdress, "cell 0 2 3 1,alignx left,aligny center");
+		panel.add(emailTextField, "cell 4 2 6 1,aligny top");
+		panel.add(logopanel, "cell 4 1 3 1,grow");
+
+		cp.add(panel);
+
 		this.getContentPane().add(cp);
 		// this.setVisible(true);
+	}
+
+	/**
+	 * this methode update the response Label 'respLabel'
+	 */
+	private void checkConnection() {
+		new ServerConnection();
+		respLabel.setText("Connection to server...");
+		respLabel.setForeground(new Color(0, 100, 0));
+		String hashedPass = new String();
+		if (savedIdentity && !passwordChanged) {
+			hashedPass = lt.isSaved(emailTextField.getText());
+		} else {
+			hashedPass = new HashPassword(lt.arrayToString(passwordField
+					.getPassword())).getHashed();
+		}
+		User u = ClientRequests.connectUser(this.emailTextField.getText(),
+				hashedPass);
+		if (u == null) {
+			respLabel.setText("Connection impossible...");
+			respLabel.setForeground(new Color(100, 0, 0));
+		} else if (u.getId() != -1) {
+			if (chckbxRememberMe.isSelected()
+					&& !lt.arrayToString(passwordField.getPassword()).equals(
+							"********")) {
+				lt.savePassword(
+						emailTextField.getText(),
+						new HashPassword(lt.arrayToString(passwordField
+								.getPassword())).getHashed());
+			}
+			dispose();
+			new CurrentUser(u);
+			this.initMainWindow();
+		} else if (u.getId() == -1) {
+			savedIdentity = false;
+			respLabel
+					.setText("Wrong password or wrong email, Please try again!");
+			respLabel.setForeground(new Color(200, 0, 0));
+		}
+	}
+
+	private void initMainWindow() {
+		MainWindow.initFrame();
+		this.w = new MainWindow(); // Initialize new Main Window
+
+		// Setting the frame Title
+		this.w.getFrame()
+				.setTitle("JMBS Client : " + CurrentUser.getFullName());
+		// Empty the List of messages if not
+		this.w.initMsgListTL();
+		// set the id of the last received message at 0
+		this.w.getTLPanel().setLastIdMsg(0);
+		// remove all the comptent from the timeline panel
+		this.w.getTLPanel().removeAll();
+		this.w.getTLPanel().updateUI();
+		// Repante and revalidate the timeline panel
+		this.w.getTLPanel().repaint();
+		this.w.getTLPanel().revalidate();
+		// check for new messages from the server and display them on the
+		// timeline panel
+		MainWindow.checkCacheMsgs();
+		MainWindow.checkNewMessages(TimeLinePanel.idLastMessage);
+		// Resetting the profile panel
+		this.w.resetProfilePanel();
+		// setting the menubar
+		this.w.setMenuBar();
+		// Display the main frame
+		this.w.getFrame().setVisible(true);
 	}
 
 }
--- jmbs_maven/Client/src/main/java/jmbs/client/Graphics/RegisterFrame.java
+++ jmbs_maven/Client/src/main/java/jmbs/client/Graphics/RegisterFrame.java
@@ -18,16 +18,34 @@
 package jmbs.client.Graphics;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.util.regex.Pattern;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import jmbs.client.ClientRequests;
+import jmbs.client.ServerConnection;
 import jmbs.client.DataTreatment.FramesConf;
+import jmbs.client.DataTreatment.HashPassword;
+import jmbs.client.Graphics.images.ImagePanel;
+import jmbs.client.Graphics.others.SayToUser;
+import jmbs.common.User;
+import net.miginfocom.swing.MigLayout;
 
 /**
  * @author <a href="mailto:younes.cheikh@gmail.com">Younes CHEIKH</a>
@@ -43,6 +61,12 @@ public class RegisterFrame extends JFrame {
 	private static final long serialVersionUID = -7428710889628300587L;
 	private JPanel contentPane;
 	private static Point point = new Point();
+	private JPasswordField confirmPasswordField;
+	private JPasswordField passwordField;
+	private JTextField emailTextField;
+	private JTextField fnameTextField;
+	private JTextField nameTextField;
+	private JLabel lblResp;
 
 	/**
 	 * Create the frame.
@@ -79,8 +103,235 @@ public class RegisterFrame extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 
-		RegisterPanel panel = new RegisterPanel(this);
+		JPanel panel = new JPanel();
+
+		JLabel lblFirstName = new JLabel("First Name:");
+
+		JLabel lblLastName = new JLabel("Last Name:");
+
+		JLabel lblEmailAddress = new JLabel("Email Address:");
+
+		JLabel lblPassword = new JLabel("Password:");
+
+		JLabel lblConfirmPassword = new JLabel("Confirm Password:");
+
+		confirmPasswordField = new JPasswordField();
+		confirmPasswordField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				analysis();
+			}
+		});
+
+		passwordField = new JPasswordField();
+		passwordField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				analysis();
+			}
+		});
+
+		emailTextField = new JTextField();
+		emailTextField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				analysis();
+			}
+		});
+		emailTextField.setColumns(10);
+
+		fnameTextField = new JTextField();
+		fnameTextField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				analysis();
+			}
+		});
+		fnameTextField.setColumns(10);
+
+		nameTextField = new JTextField();
+		nameTextField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				analysis();
+			}
+		});
+		nameTextField.setColumns(10);
+
+		nameTextField.setBorder(BorderFactory.createLineBorder(null));
+		fnameTextField.setBorder(BorderFactory.createLineBorder(null));
+		emailTextField.setBorder(BorderFactory.createLineBorder(null));
+		passwordField.setBorder(BorderFactory.createLineBorder(null));
+		confirmPasswordField.setBorder(BorderFactory.createLineBorder(null));
+
+		lblResp = new JLabel("  ");
+
+		JSeparator separator = new JSeparator();
+
+		JButton btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				analysis();
+			}
+		});
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+
+		JSeparator separator_1 = new JSeparator();
+
+		JLabel lblWelcomeToJmbs = new JLabel("Welcome to JMBS");
+		lblWelcomeToJmbs.setFont(new Font("Lucida Grande", Font.BOLD, 16));
+
+		ImagePanel impanel = new ImagePanel("/img/jmbslogo_small.png");
+		panel.setLayout(new MigLayout("", "[127px][18px][139px][1px][163px]",
+				"[20px][156px][18px][18px][18px][12px][18px][18px][29px][12px][16px]"));
+		panel.add(lblResp, "cell 0 10 5 1,growx,aligny top");
+		panel.add(separator, "cell 0 9 5 1,growx,aligny top");
+		panel.add(lblPassword, "cell 0 6,alignx left,aligny center");
+		panel.add(passwordField, "cell 2 6 3 1,growx,aligny top");
+		panel.add(lblConfirmPassword, "cell 0 7,alignx right,aligny center");
+		panel.add(btnCancel, "cell 2 8,alignx left,aligny top");
+		panel.add(btnRegister, "cell 4 8,alignx left,aligny top");
+		panel.add(confirmPasswordField, "cell 2 7 3 1,growx,aligny top");
+		panel.add(separator_1, "cell 0 5 5 1,growx,aligny top");
+		panel.add(lblEmailAddress, "cell 0 4,alignx left,aligny center");
+		panel.add(emailTextField, "cell 2 4 3 1,growx,aligny top");
+		panel.add(lblLastName, "cell 0 3,alignx left,aligny center");
+		panel.add(fnameTextField, "cell 2 3 3 1,growx,aligny top");
+		panel.add(lblFirstName, "cell 0 2,alignx left,aligny center");
+		panel.add(nameTextField, "cell 2 2 3 1,growx,aligny top");
+		panel.add(impanel, "cell 2 1,grow");
+		panel.add(lblWelcomeToJmbs, "cell 2 0,alignx left,aligny top");
+
 		contentPane.add(panel, BorderLayout.CENTER);
+	}
+
+	/**
+	 * @return true if the nameTextfiled is not empty
+	 */
+	private boolean rightName() {
+		if (nameTextField.getText().equals(""))
+			return false;
+		return true;
+	}
+
+	/**
+	 * @return true if fnameTextfield is not empty
+	 */
+	private boolean rightFName() {
+		if (fnameTextField.getText().equals(""))
+			return false;
+		return true;
+	}
+
+	/**
+	 * @return true if the emailTextfield contains a valid adress mail
+	 */
+	private boolean rightEmail() {
+		boolean correctMail = Pattern.matches(
+				"^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)+$",
+				emailTextField.getText());
+		return correctMail;
+	}
+
+	/**
+	 * @param pf
+	 *            passwordfiled
+	 * @return true if password length > 4 charcters
+	 */
+	private boolean rightPassword(JPasswordField pf) {
+		return pf.getPassword().length >= 4;
+	}
+
+	/**
+	 * @return true if the passwordFiled and confirmedPasswordfiled contain the
+	 *         same password
+	 */
+	private boolean confirmedPassword() {
+		String strPass = new HashPassword(
+				listToString(passwordField.getPassword())).getHashed();
+		String strConfirmedPass = new HashPassword(
+				listToString(confirmPasswordField.getPassword())).getHashed();
+		return strPass.equals(strConfirmedPass);
+	}
+
+	/**
+	 * @param list
+	 *            array of chars
+	 * @return string converted from array
+	 */
+	private String listToString(char[] list) {
+		String retStr = new String();
+		for (char c : list) {
+			retStr += c;
+		}
+		return retStr;
+	}
+
+	/**
+	 * confirm the entred data by the user in the text fields
+	 */
+	private void analysis() {
+		new ServerConnection();
+		nameTextField.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		fnameTextField.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		emailTextField.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		passwordField.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		confirmPasswordField.setBorder(BorderFactory
+				.createLineBorder(Color.GREEN));
+		boolean correctFields = true;
+		if (!rightName()) {
+			nameTextField.setBorder(BorderFactory.createLineBorder(Color.RED));
+			correctFields = false;
+		}
+		if (!rightFName()) {
+			fnameTextField.setBorder(BorderFactory.createLineBorder(Color.RED));
+			correctFields = false;
+		}
+		if (!rightEmail()) {
+			emailTextField.setBorder(BorderFactory.createLineBorder(Color.RED));
+			correctFields = false;
+		}
+		if (!rightPassword(passwordField)) {
+			passwordField.setBorder(BorderFactory.createLineBorder(Color.RED));
+			correctFields = false;
+		}
+		if (!rightPassword(passwordField) || !confirmedPassword()) {
+			passwordField.setBorder(BorderFactory.createLineBorder(Color.RED));
+			confirmPasswordField.setBorder(BorderFactory
+					.createLineBorder(Color.RED));
+			correctFields = false;
+		}
+		if (!correctFields) {
+			lblResp.setText("Please correct the selected fields");
+			lblResp.setForeground(new Color(255, 0, 0));
+		} else {
+			lblResp.setText("Connection to server...");
+			lblResp.setForeground(new Color(0, 100, 0));
+			boolean emailAvailable = false;
+
+			emailAvailable = ClientRequests.createUser(
+					new User(nameTextField.getText(), fnameTextField.getText(),
+							emailTextField.getText()), new HashPassword(
+							listToString(passwordField.getPassword()))
+							.getHashed());
+			if (!emailAvailable) {
+				emailTextField.setBorder(BorderFactory
+						.createLineBorder(Color.RED));
+				lblResp.setText("Email in used!");
+				lblResp.setForeground(new Color(255, 0, 0));
+
+			} else {
+				SayToUser
+						.success(
+								"Congratulations!",
+								"Thanks "
+										+ nameTextField.getText()
+										+ "! your registration has been successful. Now you can login using your email and password.");
+				dispose();
+			}
+
+		}
 	}
 
 }
